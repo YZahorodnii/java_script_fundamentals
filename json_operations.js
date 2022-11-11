172f18c25840cbdf00bcafbcb0a58b5dd8a3e7dc
YZahorodnii <yzagorodniy@gmail.com>
2022-11-11 11:59:03 +0000

    Studied how to operate with dictionaries and arrays using for loops

---
+++ json_operations.js
@@ -0,0 +1,372 @@
+
+var new_json = [
+                 {
+                   "_id": "636acd4ba58b7a392f8fac3f",
+                   "index": 0,
+                   "guid": "4adf4a1f-fc49-4619-ad1c-d50b37be2c9c",
+                   "isActive": false,
+                   "balance": "$3,324.14",
+                   "picture": "http://placehold.it/32x32",
+                   "age": 35,
+                   "eyeColor": "blue",
+                   "name": "Noelle Bond",
+                   "gender": "female",
+                   "company": "MACRONAUT",
+                   "email": "noellebond@macronaut.com",
+                   "phone": "+1 (966) 469-2340",
+                   "address": "806 Schweikerts Walk, Osmond, Puerto Rico, 8441",
+                   "about": "Sit in ut ad consequat quis minim dolore irure laboris ex. Pariatur minim dolor proident officia cupidatat nisi. Enim eu esse velit qui culpa culpa anim proident do pariatur deserunt consectetur cillum. Deserunt cillum fugiat ex anim deserunt reprehenderit veniam enim ea. Eiusmod dolore cillum elit incididunt deserunt aute ut ea do et et occaecat veniam. Ullamco fugiat dolor anim non occaecat.\r\n",
+                   "registered": "2019-05-21T03:33:46 -01:00",
+                   "latitude": -33.130272,
+                   "longitude": 131.777148,
+                   "tags": [
+                     "incididunt",
+                     "quis",
+                     "amet",
+                     "sint",
+                     "commodo",
+                     "ea",
+                     "eu"
+                   ],
+                   "friends": [
+                     {
+                       "id": 0,
+                       "name": "Bolton Hernandez"
+                     },
+                     {
+                       "id": 1,
+                       "name": "Harriett Newton"
+                     },
+                     {
+                       "id": 2,
+                       "name": "Consuelo Nicholson"
+                     }
+                   ],
+                   "greeting": "Hello, Noelle Bond! You have 7 unread messages.",
+                   "favoriteFruit": "apple"
+                 },
+                 {
+                   "_id": "636acd4b8e689bf40c2942a4",
+                   "index": 1,
+                   "guid": "de1b60e4-5980-41cf-ae19-21c64ebe4f7f",
+                   "isActive": false,
+                   "balance": "$3,886.97",
+                   "picture": "http://placehold.it/32x32",
+                   "age": 31,
+                   "eyeColor": "green",
+                   "name": "Vonda Dorsey",
+                   "gender": "female",
+                   "company": "ASSURITY",
+                   "email": "vondadorsey@assurity.com",
+                   "phone": "+1 (878) 597-3658",
+                   "address": "401 Middagh Street, Gardners, South Carolina, 2311",
+                   "about": "Duis amet laborum labore cupidatat adipisicing labore nulla. Reprehenderit ex pariatur esse ad qui veniam eiusmod in quis eiusmod mollit eu. Nulla esse sit reprehenderit incididunt eiusmod ea.\r\n",
+                   "registered": "2021-07-22T11:22:06 -01:00",
+                   "latitude": -4.684963,
+                   "longitude": 97.213572,
+                   "tags": [
+                     "labore",
+                     "amet",
+                     "cillum",
+                     "aliquip",
+                     "aliquip",
+                     "officia",
+                     "deserunt"
+                   ],
+                   "friends": [
+                     {
+                       "id": 0,
+                       "name": "Morrow Ruiz"
+                     },
+                     {
+                       "id": 1,
+                       "name": "Deirdre Sellers"
+                     },
+                     {
+                       "id": 2,
+                       "name": "Hoover Smith"
+                     }
+                   ],
+                   "greeting": "Hello, Vonda Dorsey! You have 1 unread messages.",
+                   "favoriteFruit": "strawberry"
+                 },
+                 {
+                   "_id": "636acd4b81b84087c3599d70",
+                   "index": 2,
+                   "guid": "fb1ea1f5-ba17-49b1-9895-9cc9dbfb243f",
+                   "isActive": false,
+                   "balance": "$1,269.76",
+                   "picture": "http://placehold.it/32x32",
+                   "age": 21,
+                   "eyeColor": "green",
+                   "name": "Alejandra Osborne",
+                   "gender": "female",
+                   "company": "HAWKSTER",
+                   "email": "alejandraosborne@hawkster.com",
+                   "phone": "+1 (820) 443-3362",
+                   "address": "398 Friel Place, Saticoy, Texas, 3058",
+                   "about": "Ipsum ullamco et eu amet cupidatat fugiat minim ad dolor exercitation pariatur ad non. Lorem consectetur laborum ut qui est dolor qui. Ullamco ipsum aliqua pariatur quis et reprehenderit enim ad occaecat ipsum esse ea. Enim ullamco exercitation mollit labore voluptate pariatur sint reprehenderit ullamco duis dolore sint. Sit veniam aute sit fugiat dolore pariatur voluptate voluptate ut est aliquip. Minim consectetur incididunt et sint Lorem fugiat sunt veniam anim eiusmod labore nisi.\r\n",
+                   "registered": "2022-07-19T01:29:04 -01:00",
+                   "latitude": -67.45817,
+                   "longitude": 37.167705,
+                   "tags": [
+                     "minim",
+                     "commodo",
+                     "ad",
+                     "ut",
+                     "cupidatat",
+                     "laborum",
+                     "id"
+                   ],
+                   "friends": [
+                     {
+                       "id": 0,
+                       "name": "Wood Britt"
+                     },
+                     {
+                       "id": 1,
+                       "name": "Alford Hayden"
+                     },
+                     {
+                       "id": 2,
+                       "name": "Roxanne Morrow"
+                     }
+                   ],
+                   "greeting": "Hello, Alejandra Osborne! You have 3 unread messages.",
+                   "favoriteFruit": "strawberry"
+                 },
+                 {
+                   "_id": "636acd4bcf90c7ee18880b5c",
+                   "index": 3,
+                   "guid": "9aab1cfd-86a9-4813-9b10-31b8fd5cbfce",
+                   "isActive": false,
+                   "balance": "$2,435.20",
+                   "picture": "http://placehold.it/32x32",
+                   "age": 28,
+                   "eyeColor": "brown",
+                   "name": "Helena Frazier",
+                   "gender": "female",
+                   "company": "TRANSLINK",
+                   "email": "helenafrazier@translink.com",
+                   "phone": "+1 (856) 520-2663",
+                   "address": "124 Church Lane, Farmington, District Of Columbia, 6635",
+                   "about": "Enim excepteur sunt aliquip cupidatat est est irure officia ea ea anim aute esse dolor. Id ea laboris sit ea incididunt do fugiat. Et consectetur reprehenderit elit et proident nostrud commodo culpa. Eu anim ad esse minim veniam.\r\n",
+                   "registered": "2022-01-20T10:19:14 -00:00",
+                   "latitude": -21.60291,
+                   "longitude": 107.242238,
+                   "tags": [
+                     "eiusmod",
+                     "tempor",
+                     "ex",
+                     "ullamco",
+                     "amet",
+                     "do",
+                     "proident"
+                   ],
+                   "friends": [
+                     {
+                       "id": 0,
+                       "name": "Huber Phelps"
+                     },
+                     {
+                       "id": 1,
+                       "name": "Rios Petersen"
+                     },
+                     {
+                       "id": 2,
+                       "name": "Barrera Marshall"
+                     }
+                   ],
+                   "greeting": "Hello, Helena Frazier! You have 9 unread messages.",
+                   "favoriteFruit": "strawberry"
+                 },
+                 {
+                   "_id": "636acd4beda7cb44dcd523da",
+                   "index": 4,
+                   "guid": "01d2c699-b616-49bb-9fa5-e9f58650387b",
+                   "isActive": true,
+                   "balance": "$1,475.67",
+                   "picture": "http://placehold.it/32x32",
+                   "age": 36,
+                   "eyeColor": "brown",
+                   "name": "Kate Barker",
+                   "gender": "female",
+                   "company": "TYPHONICA",
+                   "email": "katebarker@typhonica.com",
+                   "phone": "+1 (958) 434-3647",
+                   "address": "312 Remsen Avenue, Forestburg, Oregon, 937",
+                   "about": "Excepteur dolor deserunt enim laborum ut ipsum amet excepteur. Consectetur voluptate aute fugiat sunt. Esse elit adipisicing aliquip culpa irure consequat fugiat veniam dolore. Voluptate non labore occaecat non elit proident consectetur. Nostrud magna esse cillum anim aute sunt reprehenderit aliqua exercitation ipsum exercitation irure pariatur sint. Elit consequat laborum elit ex amet sint quis magna deserunt et duis. Pariatur voluptate magna eu eu reprehenderit Lorem dolor.\r\n",
+                   "registered": "2017-05-31T05:53:02 -01:00",
+                   "latitude": 20.748383,
+                   "longitude": 46.536385,
+                   "tags": [
+                     "sint",
+                     "aliqua",
+                     "aute",
+                     "deserunt",
+                     "quis",
+                     "anim",
+                     "sint"
+                   ],
+                   "friends": [
+                     {
+                       "id": 0,
+                       "name": "Paige Hays"
+                     },
+                     {
+                       "id": 1,
+                       "name": "Roach Rodriguez"
+                     },
+                     {
+                       "id": 2,
+                       "name": "Lopez Walls"
+                     }
+                   ],
+                   "greeting": "Hello, Kate Barker! You have 9 unread messages.",
+                   "favoriteFruit": "banana"
+                 },
+                 {
+                   "_id": "636acd4b84bc61a4aeb00037",
+                   "index": 5,
+                   "guid": "47d894e3-4301-485f-b4d8-68ae8c055aad",
+                   "isActive": true,
+                   "balance": "$1,336.72",
+                   "picture": "http://placehold.it/32x32",
+                   "age": 34,
+                   "eyeColor": "blue",
+                   "name": "Ellison Holmes",
+                   "gender": "male",
+                   "company": "FUELTON",
+                   "email": "ellisonholmes@fuelton.com",
+                   "phone": "+1 (987) 405-3381",
+                   "address": "421 Quentin Road, Twilight, Federated States Of Micronesia, 1065",
+                   "about": "Sit ut laboris incididunt voluptate mollit consectetur ipsum sit. Elit do ea eiusmod consectetur eu sunt. Sint aliqua magna consectetur adipisicing reprehenderit pariatur id ea duis tempor minim incididunt amet cillum.\r\n",
+                   "registered": "2015-11-08T12:37:31 -00:00",
+                   "latitude": 3.26879,
+                   "longitude": 98.820368,
+                   "tags": [
+                     "eiusmod",
+                     "ad",
+                     "duis",
+                     "sint",
+                     "minim",
+                     "quis",
+                     "ipsum"
+                   ],
+                   "friends": [
+                     {
+                       "id": 0,
+                       "name": "Joyner Griffith"
+                     },
+                     {
+                       "id": 1,
+                       "name": "Lena Hewitt"
+                     },
+                     {
+                       "id": 2,
+                       "name": "Sharlene Steele"
+                     }
+                   ],
+                   "greeting": "Hello, Ellison Holmes! You have 8 unread messages.",
+                   "favoriteFruit": "apple"
+                 },
+                 {
+                   "_id": "636acd4b48b260720f27afab",
+                   "index": 6,
+                   "guid": "84b356b8-d9f8-47c5-9c73-93301feb8c86",
+                   "isActive": true,
+                   "balance": "$1,795.79",
+                   "picture": "http://placehold.it/32x32",
+                   "age": 24,
+                   "eyeColor": "brown",
+                   "name": "Ashley Gentry",
+                   "gender": "male",
+                   "company": "SUPPORTAL",
+                   "email": "ashleygentry@supportal.com",
+                   "phone": "+1 (935) 406-2343",
+                   "address": "878 Meadow Street, Chalfant, Kansas, 7361",
+                   "about": "Elit sint ad labore culpa ad pariatur occaecat duis ex minim cupidatat. Incididunt reprehenderit officia veniam dolore est do dolor ullamco eu minim Lorem. Aliqua labore velit in sunt pariatur dolor commodo proident minim. Est nisi ullamco tempor nostrud nulla aute reprehenderit sunt est aliqua aute fugiat. Duis minim ipsum est non deserunt Lorem deserunt officia dolore ut eu. Ipsum consectetur incididunt qui do nulla id. Culpa pariatur laborum cupidatat Lorem quis fugiat ad.\r\n",
+                   "registered": "2022-05-07T09:27:54 -01:00",
+                   "latitude": -51.931628,
+                   "longitude": -102.031578,
+                   "tags": [
+                     "ullamco",
+                     "magna",
+                     "officia",
+                     "nostrud",
+                     "ut",
+                     "ea",
+                     "commodo"
+                   ],
+                   "friends": [
+                     {
+                       "id": 0,
+                       "name": "Sonya Potts"
+                     },
+                     {
+                       "id": 1,
+                       "name": "Jordan Cherry"
+                     },
+                     {
+                       "id": 2,
+                       "name": "Jessie Holman"
+                     }
+                   ],
+                   "greeting": "Hello, Ashley Gentry! You have 8 unread messages.",
+                   "favoriteFruit": "apple"
+                 }
+               ]
+
+// return "_id"
+for (var i = 0; i < new_json.length; i++) {
+    console.log(new_json[i]["_id"]);
+
+// return "_id" and "name"
+
+for (var i = 0; i < new_json.length; i++) {
+    reconsole.log(new_json[i]["_id"]);
+     for (var j = 0; j < Object.keys(new_json[i]["friends"]).length; j++) {
+        console.log(new_json[i]["friends"][j]["name"]);
+    }
+}
+
+// return "tags" for each "_id"
+
+for (var i = 0; i < new_json.length; i++) {
+    console.log(new_json[i]["_id"]);
+    for (var j = 0; j < Object.keys(new_json[i]["tags"]).length; j++) {
+        console.log(new_json[i]["tags"][j]);
+    }
+}
+
+
+//  return [{"_id": {"friends": [all names]}}]
+
+for (var i in new_json) {
+    var new_dict = {};
+    new_dict[new_json[i]["_id"]] = {"friends": []};
+    for (var j in new_json[i]["friends"]) {
+        var names_dict = new_json[i]["friends"][j]["name"];
+        var new_obj = [];
+        new_dict[new_json[i]["_id"]]["friends"].push(names_dict);
+        new_obj.push(new_dict);
+    }
+    console.log(new_obj);
+}
+
+
+//  return [{"_id": {"friends": [all names]}}]
+
+for (var i = 0; i < new_json.length; i++) {
+    var new_dict = {};
+    console.log(typeof(new_dict));
+    new_dict[new_json[i]["_id"]] = {"friends": []};
+    for (j = 0; j < new_json[i]["friends"].length; j++) {
+        var new_obj = [];
+        var new_name = new_json[i]["friends"][j]["name"];
+        new_dict[new_json[i]["_id"]]["friends"].push(new_name);
+        new_obj.push(new_dict);
+
+    }
+    console.log(JSON.stringify(new_obj))
+}
