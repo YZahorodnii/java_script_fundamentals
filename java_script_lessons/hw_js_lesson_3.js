89d9fc38aba2bcf13f7d8429433b915fba98956a
YZahorodnii <yzagorodniy@gmail.com>
2023-02-08 16:10:21 +0000

    Started additional homework for lesson 3

--- java_script_lessons/hw_js_lesson_3.js
+++ java_script_lessons/hw_js_lesson_3.js
@@ -131,3 +131,176 @@ for (const i in users) {
         console.log(users[i]);
     }
 };
+
+                // ADDITIONAL TASK1
+
+// --створити масив з:
+//     - з 5 числових значень
+// - з 5 стічкових значень
+// - з 5 значень стрічкового, числового та булевого типу
+// - та вивести його в консоль
+let arr = [];
+for (let i = 0; i < 5; i++) {
+    arr.push(i);
+};
+console.log(arr);
+
+let arr1 = [];
+for (let i = 0; i < 5; i++) {
+    arr1.push('string')
+};
+console.log(arr1);
+
+let arr2 = [];
+for (let i = 0; i < 5; i++) {
+    arr2.push(i);
+    arr2.push('string')
+    arr2.push(false)
+};
+console.log(arr2);
+
+// -- Створити пустий масив. Наповнити його будь-якими значеннями звертаючись
+// до конкретного індексу. Вивести в консоль
+let newArr = [];
+newArr[0] = 'string';
+newArr[1] = '5';
+newArr[2] = true;
+newArr[3] = false;
+console.log(newArr);
+// - є масив [2,17,13,6,22,31,45,66,100,-18] :
+// 1. перебрати його циклом while
+let array = [2,17,13,6,22,31,45,66,100,-18];
+var i = 0;
+while (i < array.length) {
+    console.log(array[i]);
+    i++;
+};
+//     2. перебрати його циклом for
+for (i = 0; i < array.length; i++) {
+    console.log(array[i]);
+};
+//     3. перебрати циклом while та вивести  числа тільки з непарним індексом
+let a = 1;
+while (a < array.length) {
+    console.log(array[a]);
+    a += 2;
+};
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+for (let i = 1; i < array.length; i += 2) {
+    console.log(array[i]);
+};
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
+let b = 0;
+while (b < array.length) {
+    console.log(array[b]);
+    b += 2;
+};
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
+for (let i = 0; i < array.length; i += 2) {
+    console.log(array[i]);
+};
+// 7. замінити кожне число кратне 3 на слово "okten"
+// for (i = 0; i < array.length; i++) {
+//     if (array[i] % 3 == 0) {
+//         array[i] = 'okten';
+//     }
+// };
+// console.log(array);
+// 8. вивести масив в зворотньому порядку.
+for (i = array.length-1; i >= 0; i--) {
+    console.log(array[i]);
+};
+// 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
+let c = array.length-1;
+while (c >= 0) {
+    console.log(array[c]);
+    c--;
+};
+
+for (i = array.length-1; i >= 0; i--) {
+    console.log(array[i]);
+};
+
+let d = array.length-1;
+while (d >= 0) {
+    console.log(array[d]);
+    d -= 2;
+};
+
+let e = 0;
+while (e < array.length) {
+    console.log(array[e]);
+    e += 2;
+};
+// - Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+let newArray = [1,2,3,4,5,6,7,8,9,10];
+for (const number of newArray) {
+    console.log(newArray[number-1]);;
+};
+// - Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
+let letArray = ['aa','bb','cc','dd','ee','ff','gg','hh','ii','jj'];
+
+for (let j = 0; j < letArray.length; j++) {
+    console.log(letArray[j]);
+}
+// - Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+let massArr = [false, true, 4, 6, 10, 100, 'hghg', 'rtr', 4, 0];
+for (let j = 0; j < massArr.length; j++) {
+    console.log(massArr[j]);
+};
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof
+// вивести тільки булеві елементи
+for (let j = 0; j < massArr.length; j++) {
+    if (typeof(massArr[j]) == 'boolean') {
+        console.log(massArr[j]);
+    }
+};
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof
+// вивести тільки числові елементи
+for (let j = 0; j < massArr.length; j++) {
+    if (typeof(massArr[j]) == 'number') {
+        console.log(massArr[j]);
+    }
+};
+// - Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof
+// вивести тільки рядкові елементи
+for (let j = 0; j < massArr.length; j++) {
+    if (typeof(massArr[j]) == 'string') {
+        console.log(massArr[j]);
+    }
+};
+// - Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до
+// конкретних індексів.
+// Вивести в консоль всі його елементи в циклі.
+let emptyArr = [];
+emptyArr[0]= 66;
+emptyArr[1] = 99
+emptyArr[2] = 'hower'
+emptyArr[3] = 'bull'
+emptyArr[4] = false
+emptyArr[5] = 'cool'
+emptyArr[6] = 'true'
+emptyArr[7] = true
+emptyArr[8] = 0
+emptyArr[9] = 123
+for (let j = 0; j < emptyArr.length; j++) {
+    console.log(emptyArr[j]);
+};
+// - Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та
+// document.write
+
+// - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та
+// document.write
+// - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та
+// document.write
+// - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+// - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+//
+//
+//
+// стоврити масив книжок (назва, кількість сторінок, автори , жанри).
+// -знайти наібльшу книжку.
+// - знайти книжку/ки з найбільшою кількістю жанрів
+// - знайти книжку/ки з найдовшою назвою
+// - знайти книжку/ки які писали 2 автори
+// - знайти книжку/ки які писав 1 автор
