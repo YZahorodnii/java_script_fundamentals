f7764b04d51fa79549f22ab712658da7d2c01119
YZahorodnii <yzagorodniy@gmail.com>
2023-05-03 09:43:14 +0100

    Refactored code

---
+++ app.js
@@ -0,0 +1,14 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navList');
+    const navLinks = document.querySelector('.navList li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active')
+    })
+
+    navLinks.forEach()
+
+}
+
+navSlide();
